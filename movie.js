e922712a01f952266bb03d2c2473a81a8b880558
fatimaarif19 <fatima.arif13071989@gmail.com>
2024-12-01 00:17:52 +0530

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,33 @@
+const searchBtn = document.getElementById("searchBtn");
+const movieInput = document.getElementById("movieInput");
+const movieTitle = document.getElementById("movieTitle");
+const movieYear = document.getElementById("movieYear");
+const moviePlot = document.getElementById("moviePlot");
+
+const apiKey = 'eb1fd5dc';
+
+searchBtn.addEventListener("click", function() {
+    const movieName = movieInput.value.trim(); //trim removes any like space in the input so code works yay
+    if (movieName === "") {
+        alert("enter a movie name first!"); //if they aint typing stuff n pressing enter, u get an alert
+        return;
+    }
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const url = `https://www.omdbapi.com/?t=${movieName}&apikey=${apiKey}`;
+
+    fetch(proxy + url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.Response === "True") {
+                movieTitle.textContent = data.Title;
+                movieYear.textContent = `Year: ${data.Year}`;
+                moviePlot.textContent = data.Plot;                    
+            } else {
+                alert("aww sorry movie not found 😔.");
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+            alert("oops! something is wrong.");
+        });
+});
